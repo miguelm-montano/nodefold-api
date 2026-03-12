a9b11386745e82de01db6da1b75d654519f774be
Miguel Montaño <miiguel2m@gmail.com>
2026-03-12 16:22:06 +0100

    feat: folder creation validation tests

--- tests/Feature/FolderTest.php
+++ tests/Feature/FolderTest.php
@@ -51,4 +51,60 @@ class FolderTest extends TestCase
             'parent_id' => $folder->id
         ]);
     }
+
+    public function test_auhthenticated_user_cannot_create_a_subfolder_inside_another_subfolder(): void {
+
+        $user = User::factory()->create();
+
+        Passport::actingAs($user);
+
+        $folder = Folder::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $subfolder = Folder::factory()->create([
+            'user_id' => $user->id,
+            'parent_id' => $folder->id
+        ]);
+
+        $response = $this->postJson('api/folders/' . $subfolder->id . '/folders', [
+            'name' => 'Photos'
+        ]);
+
+        $response->assertStatus(403);
+    }
+
+    public function test_a_folder_cannot_be_created_without_name(): void {
+
+        $user = User::factory()->create();
+
+        Passport::actingAs($user);
+
+        $response = $this->postJson('/api/folders', [
+            'name' => null
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_authenticated_user_cannot_create_a_subfolder_in_a_folders_of_other(): void {
+
+        $user = User::factory()->create();
+
+        $otherUser = User::factory()->create();
+
+        Passport::actingAs($user);
+        $folder = Folder::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        Passport::actingAs($otherUser);
+        $response = $this->postJson('/api/folders/' . $folder->id . '/folders', [
+            'name' => 'Explode'
+        ]);
+
+        $response->assertStatus(404);
+
+
+    }
 }

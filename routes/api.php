3b0587482078803c85e165ffe633cc475cfbc010
Miguel Montaño <miiguel2m@gmail.com>
2026-03-18 13:36:44 +0100

    feat: routes protected with isAdmin middleware and updated

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,9 @@ Route::prefix('v1')->group(function () {
         Route::get('tags', [TagController::class, 'index']);
     });
 
-        Route::middleware(['auth:api', 'isAdmin'])->group(function () {
+    Route::middleware(['auth:api', 'isAdmin'])->group(function () {
         Route::get('/admin/users', [AdminController::class, 'index']);
+        Route::delete('/admin/users/{id}', [AdminController::class, 'destroy']);
+        Route::get('/admin/stats', [AdminController::class, 'stats']);
     }); 
 });
